4da9a98a164b978959a0d6b2f22e3d5f77706cc3
profilenamegoeshere <jcollingreene@gmail.com>
2025-09-09 18:08:51 -0400

    Basic dialogue should work
    The basics of the dialogue system should work now, but still lacking things like resetting the timer if you let go of space in the first 1 sec, second choice doesn't have a buffer between pressing space and letting you choose, and it doesn't switch the name to whoever is speaking.

--- VCGJ spring 25/Assets/DialogueSystem.cs
+++ VCGJ spring 25/Assets/DialogueSystem.cs
@@ -8,14 +8,16 @@ using UnityEngine;
 
 public enum Branch
 {
-    Null,
     Topaz1,
     Player1,
+    Tutorial,
+    PreChoice1,
     TopazRes11,
     TopazRes12,
     TopazRes13,
     Topaz2,
     Player2,
+    PreChoice2,
     TopazRes21,
     TopazRes22,
     TopazRes23
@@ -75,24 +77,27 @@ public class DialogueSystem : MonoBehaviour
         linesTopazRes12 = Resources.Load<TextAsset>("Topaz Response 1-2").text.Split('\n');
         linesTopazRes13 = Resources.Load<TextAsset>("Topaz Response 1-3").text.Split('\n');
         linesTopaz2 = Resources.Load<TextAsset>("Topaz 2").text.Split('\n');
-        linesPlayer2 = Resources.Load<TextAsset>("Player 1").text.Split('\n');
+        linesPlayer2 = Resources.Load<TextAsset>("Player 2").text.Split('\n');
         linesTopazRes21 = Resources.Load<TextAsset>("Topaz Response 2-1").text.Split('\n');
         linesTopazRes22 = Resources.Load<TextAsset>("Topaz Response 2-2").text.Split('\n');
         linesTopazRes23 = Resources.Load<TextAsset>("Topaz Response 2-3").text.Split('\n');
 
         displayLines[0] = linesTopaz1[0];
-        //for(int i=0; i < linesTopaz1.Length; i++)
-        //{
-        //    Debug.Log(linesTopaz1[i]);
-        //}
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        int timerLimit = 4;
+        if (dialogueHandler.whichBranch() == Branch.PreChoice2)
+        {
+            timerLimit = 3;
+        }
         if (dialogueHandler.isChoosing())
         {
-            if (timer < 4 && (Input.GetKey(KeyCode.Space)||timer<=1))
+            // TODO if less than 1 and let go of space just reset timer and dont choose
+            if (timer < timerLimit && (Input.GetKey(KeyCode.Space)||timer<=1))
             {
                 timer += (Time.deltaTime)/2;
                 dialogueHandler.checkChoosing(timer);
@@ -129,37 +134,72 @@ public class DialogueSystem : MonoBehaviour
 
     public class DialogueHandler
     {
-        int dialogueIter = 0;
-        Branch currBranch = Branch.Topaz1;
-        Branch nextBranch = Branch.Null;
-        Boolean choosing = false;
+        public int dialogueIter = 0;
+        public Branch currBranch = Branch.Topaz1;
+        public Branch nextBranch = Branch.PreChoice1;
+        public Boolean choosing = false;
 
         public void advanceDialogue()
         {
-            if(dialogueIter < 3 && currBranch == Branch.Topaz1)
+            if(dialogueIter < 4 && currBranch == Branch.Topaz1)
             {
                 dialogueIter++;
                 DialogueSystem.addToDisplayLines(DialogueSystem.linesTopaz1[dialogueIter]);
-            } else if (dialogueIter >= 3 && currBranch == Branch.Topaz1)
+            } 
+            else if (dialogueIter >= 4 && currBranch == Branch.Topaz1)
             {
                 currBranch = Branch.Player1;
                 DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer1[0]);
                 dialogueIter = 0;
                 choosing = true;
-            } else if(currBranch == Branch.TopazRes11)
+            } 
+            else if(currBranch == Branch.TopazRes11)
             {
                 dialogueIter++;
                 DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes11[dialogueIter]);
+                currBranch = Branch.Topaz2;
+                dialogueIter = 0;
             }
             else if (currBranch == Branch.TopazRes12)
             {
                 dialogueIter++;
                 DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes12[dialogueIter]);
+                currBranch = Branch.Topaz2;
+                dialogueIter = 0;
             }
             else if (currBranch == Branch.TopazRes13)
             {
                 dialogueIter++;
                 DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes13[dialogueIter]);
+                currBranch = Branch.Topaz2;
+                dialogueIter = 0;
+            }
+            else if (dialogueIter < 7 && currBranch == Branch.Topaz2)
+            {
+                DialogueSystem.addToDisplayLines(DialogueSystem.linesTopaz2[dialogueIter]);
+                dialogueIter++;
+            }
+            else if(dialogueIter >= 7 && currBranch == Branch.Topaz2)
+            {
+                currBranch = Branch.Player2;
+                DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer2[0]);
+                dialogueIter = 0;
+                choosing = true;
+            }
+            else if (currBranch == Branch.TopazRes21)
+            {
+                dialogueIter++;
+                DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes21[dialogueIter]);
+            }
+            else if (currBranch == Branch.TopazRes22)
+            {
+                dialogueIter++;
+                DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes22[dialogueIter]);
+            }
+            else if (currBranch == Branch.TopazRes23)
+            {
+                dialogueIter++;
+                DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes23[dialogueIter]);
             }
         }
 
@@ -168,49 +208,59 @@ public class DialogueSystem : MonoBehaviour
             choosing = false;
             // based on nextBranch, change currBranch
             currBranch = nextBranch;
+            if (nextBranch == Branch.TopazRes11 || nextBranch == Branch.TopazRes12 || nextBranch == Branch.TopazRes13)
+            {
+                nextBranch = Branch.PreChoice2;
+            }
             dialogueIter = -1;
             advanceDialogue();
-            //if(currBranch == Branch.TopazRes11)
-            //{
-            //    dialogueIter = 0;
-            //    DialogueSystem.addToDisplayLines(DialogueSystem.linesTopazRes13[dialogueIter]);
-            //    dialogueIter++;
-            //} else if (currBranch == Branch.TopazRes12)
-            //{
-            //    dialogueIter = 0;
-            //    DialogueSystem.displayLines[dialogueIter + 1] = DialogueSystem.linesTopazRes12[0];
-            //    dialogueIter++;
-            //} else
-            //{
-            //    dialogueIter = 0;
-            //    DialogueSystem.displayLines[dialogueIter + 1] = DialogueSystem.linesTopazRes13[0];
-            //    dialogueIter++;
-            //}
         }
 
         public void checkChoosing(float timer)
         {
             // change nextBranch based on timer and advance dialogue while currBranch is still in player choosing
-            if (timer < 2)
+            Debug.Log(nextBranch.ToString());
+            if (timer < 1)
             {
-                if (nextBranch == Branch.Null)
+                if (nextBranch == Branch.PreChoice1)
+                {
+                    nextBranch = Branch.Tutorial;
+                    DialogueSystem.addToDisplayLines("Press and hold space to start selecting. Let go of space to choose displayed dialogue choice.");
+                } else if(nextBranch == Branch.PreChoice2)
+                {
+                    nextBranch = Branch.TopazRes21;
+                    DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer2[0]);
+                }
+            } else if (timer < 2)
+            {
+                if (nextBranch == Branch.Tutorial)
                 {
                     nextBranch = Branch.TopazRes11;
-                    advanceDialogue();
+                    DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer1[0]);
+                }
+                else if (nextBranch == Branch.TopazRes21)
+                {
+                    nextBranch = Branch.TopazRes22;
+                    DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer2[1]);
                 }
             } else if (timer < 3)
             {
                 if (nextBranch == Branch.TopazRes11)
                 {
                     nextBranch = Branch.TopazRes12;
-                    advanceDialogue();
+                    DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer1[1]);
+                }
+                else if (nextBranch == Branch.TopazRes22)
+                {
+                    nextBranch = Branch.TopazRes23;
+                    DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer2[2]);
                 }
             } else
             {
                 if (nextBranch == Branch.TopazRes12)
                 {
                     nextBranch = Branch.TopazRes13;
-                    advanceDialogue();
+                    DialogueSystem.addToDisplayLines(DialogueSystem.linesPlayer1[2]);
                 }
             }
         }
